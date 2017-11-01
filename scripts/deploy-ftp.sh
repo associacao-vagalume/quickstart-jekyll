b282a50a3486553ed504f2bb9666a5673406b460
Camilla Gomes <cgomes@thoughtworks.com>
2017-11-01 19:42:19 -0200

    lftp configure

--- scripts/deploy-ftp.sh
+++ scripts/deploy-ftp.sh
@@ -4,14 +4,11 @@ abort() {
     echo $message
     exit -1
 }
+
 [ -z $FTP_PASS ] && abort "FTP_PASS is undefined"
 [ -z $FTP_USER ] && abort "FTP_USER is undefined"
 [ -z $FTP_SITE ] && abort "FTP_SITE is undefined"
 
-# lftp -u $FTP_USER,$FTP_PASS $FTP_SITE \
-#  -e 'set ssl:verify-certificate false ; mirror -c -e -R _site ~ ; exit'
-
-#!/bin/bash    
 HOST=$FTP_SITE
 USER=$FTP_USER
 PASS=$FTP_PASS
@@ -26,4 +23,5 @@ cd $RCD;
 mirror --reverse \
        $DELETE \
        --verbose \
-       --exclude-glob .ftpaccess"
+       --exclude-glob .ftpaccess;
+exit"
