3cebdb771aaa1a08b2d3cfda5e474857f5fc14c5
Camilla Gomes <cgomes@thoughtworks.com>
2017-11-01 17:57:22 -0200

    Mudança no metodo do FTP

--- scripts/deploy-ftp.sh
+++ scripts/deploy-ftp.sh
@@ -7,8 +7,23 @@ abort() {
 [ -z $FTP_PASS ] && abort "FTP_PASS is undefined"
 [ -z $FTP_USER ] && abort "FTP_USER is undefined"
 [ -z $FTP_SITE ] && abort "FTP_SITE is undefined"
+
 # lftp -u $FTP_USER,$FTP_PASS $FTP_SITE \
 #  -e 'set ssl:verify-certificate false ; mirror -c -e -R _site ~ ; exit'
 
- cd ./_site && find . -type f -exec curl -u $FTP_USER:$FTP_PASS \
---ftp-create-dirs -T {} $FTP_SITE/{}
+#!/bin/bash    
+HOST=$FTP_SITE
+USER=$FTP_USER
+PASS=$FTP_PASS
+FTPURL="ftp://$USER:$PASS@$HOST"
+LCD="./_site"
+RCD="~"
+DELETE="--delete"
+lftp -c "set ftp:list-options -a; set cmd:fail-exit yes; set ssl:verify-certificate false ;
+open '$FTPURL';
+lcd $LCD;
+cd $RCD;
+mirror --reverse \
+       $DELETE \
+       --verbose \
+       --exclude-glob .ftpaccess"
