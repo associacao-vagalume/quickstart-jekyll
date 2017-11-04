d6e08ecdbc45bf8f5743c5fb3a942cf319af932e
Camilla Gomes <cgomes@thoughtworks.com>
2017-11-04 18:36:15 -0200

    Alterando a forma de realizar smoke-test, incluindo o docker no stage test e adicionando o stag taggear com condições

--- scripts/smoke-test.sh
+++ scripts/smoke-test.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 # smoke-test.sh
 #
+. scripts/smoke.sh
 url=$1
 
-statusOutput=$(curl --insecure --silent -o /dev/null  -w %{http_code} $url)
-if [[ "$statusOutput" == "000" || "$statusOutput" == "40*" || "$statusOutput" == "50*" ]]; then
-    echo "$url not reachable"
-    exit 1
-fi
-echo "Check configuration OK"
+# statusOutput=$(curl --insecure --silent -o /dev/null  -w %{http_code} $url)
+# if [[ "$statusOutput" == "000" || "$statusOutput" == "40*" || "$statusOutput" == "50*" ]]; then
+#     echo "$url not reachable"
+#     exit 1
+# fi
+# echo "Check configuration OK"
+
+# URL para testar
+smoke_url_prefix $url
+
+# Testes
+smoke_url_ok "/"
+    smoke_assert_body "Welcome to Jekyll!"
+smoke_url_ok "/about"
+    smoke_assert_body "About bacana"
+smoke_report
